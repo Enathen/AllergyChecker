6ee73fb1c4d8291240a52a82964d7e41a413a1c0
Enathen <jontekaminen@hotmail.com>
2017-10-16 19:06:25 +0200

    fixed correct checkbox

--- app/src/main/java/creativeendlessgrowingceg/allergychecker/AllergyFragment.java
+++ app/src/main/java/creativeendlessgrowingceg/allergychecker/AllergyFragment.java
@@ -1,9 +1,11 @@
 package creativeendlessgrowingceg.allergychecker;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
@@ -38,7 +41,10 @@ public class AllergyFragment extends Fragment {
     private FrameLayout parentFrameLayout;
     private LinearLayout parentLinearLayout;
     private OnFragmentInteractionListener mListener;
-    private ArrayList<LinearLayout> nutsCategory = new ArrayList<>();
+    private HashMap<String,ArrayList<LinearLayout>> Categories = new HashMap<>();
+    private HashMap<String,LinearLayout> linearLayoutParents = new HashMap<>();
+    private HashMap<String,CheckBox> checkBoxes = new HashMap<>();
+
     public AllergyFragment() {
         // Required empty public constructor
     }
@@ -69,74 +75,171 @@ public class AllergyFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    //create the look
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        AllergyList allergylist = new AllergyList();
-        for (String s : allergylist.getArrayListNuts()) {
-            LinearLayout newLinearLayout = (LinearLayout) inflater.inflate(R.layout.leftmarginrowlayout, container, false);
-            TextView textview = (TextView) newLinearLayout.findViewById(R.id.textViewLeftMargin);
-            textview.setText(s);
-            nutsCategory.add(newLinearLayout);
-
-        }
-
         parentFrameLayout = (FrameLayout) inflater.inflate(R.layout.fragment_allergy, container, false);
+        //insert everything to this linear layout
         parentLinearLayout = (LinearLayout) parentFrameLayout.findViewById(R.id.linlayoutFrag);
-        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.rowlayout, container, false);
-        parentLinearLayout.addView(linearLayout);
+        AllergyList allergylist = new AllergyList();
+        addCategory(inflater,allergylist.getArrayListFish(),"Fish");
+        addCategory(inflater,allergylist.getArrayListGluten(),"Gluten");
+        addCategory(inflater,allergylist.getArrayListNuts(),"Nuts");
+        addCategory(inflater,allergylist.getArrayListSeeds(),"Seeds");
+        addCategory(inflater,allergylist.getArrayListShellfish(),"Shellfish");
 
-        LinearLayout linearLayout2 = (LinearLayout) inflater.inflate(R.layout.rowcategorylayout, container, false);
-        CheckBox checboxNuts = (CheckBox) linearLayout2.findViewById(R.id.checkBox);
-        checboxNuts.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    for (LinearLayout linearLayout : nutsCategory) {
-                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBox);
-                        newCheck.setChecked(true);
+        parentLinearLayout.addView(insertCheckboxAndImageView(inflater,"Fish",container,R.string.fish, R.drawable.fish));
+        parentLinearLayout.addView(insertCheckboxAndImageView(inflater,"Gluten",container,R.string.gluten,R.drawable.wheat));
 
-                    }
-                }else {
-                    for (LinearLayout linearLayout : nutsCategory) {
-                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBox);
-                        newCheck.setChecked(false);
-
-                    }
-                }
-            }
-        });
-        ImageView imageView = (ImageView) linearLayout2.findViewById(R.id.imageView4);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onclickDropDownList(v);
-            }
-        });
-
-        parentLinearLayout.addView(linearLayout2);
-
-
+        parentLinearLayout.addView(insertCheckboxAndImageView(inflater,"Nuts",container,R.string.nuts, R.drawable.peanut));
+        parentLinearLayout.addView(insertCheckboxAndImageView(inflater,"Seeds",container,R.string.seeds, R.drawable.wheat));
+        parentLinearLayout.addView(insertCheckboxAndImageView(inflater,"Shellfish",container,R.string.shellfish, R.drawable.wheat));
+        parentLinearLayout.addView(insertSingleAllergy(inflater,R.string.milk,container,R.drawable.car));
         return parentFrameLayout;
 
 
     }
-    public void onclickDropDownList(View v) {
+
+    private LinearLayout insertSingleAllergy(LayoutInflater inflater, final int name, ViewGroup container, int pictureId) {
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.rowlayout, container, false);
+        ((TextView) linearLayout.findViewById(R.id.textViewRow)).setText(name);
+        ((ImageView) linearLayout.findViewById(R.id.imageViewRow)).setImageResource(pictureId);
+
+        SharedPreferences settings = getContext().getSharedPreferences(getResources().getString(name), 0);
+        final SharedPreferences.Editor editor = settings.edit();
+        CheckBox checkBox = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+        checkBox.setChecked(settings.getBoolean(getResources().getString(name), false));
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                editor.putBoolean(getResources().getString(name), isChecked);
+                editor.apply();
+            }
+        });
+        return linearLayout;
+
+    }
+
+    private LinearLayout insertCheckboxAndImageView(LayoutInflater inflater, final String key, ViewGroup container, int name, int pictureId) {
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.rowcategorylayout, container, false);
+        LinearLayout linearLayoutRow = (LinearLayout) linearLayout.findViewById(R.id.linearLayoutRowCategoryHorizontal);
+        TextView textView = (TextView) linearLayout.findViewById(R.id.textViewCategory);
+        textView.setText(name);
+        final CheckBox checkboxRowCategory = (CheckBox) linearLayoutRow.findViewById(R.id.checkBoxRow);
+        ImageView imageView = (ImageView) linearLayoutRow.findViewById(R.id.dropDownList);
+        ((ImageView) linearLayoutRow.findViewById(R.id.imageViewRow)).setImageResource(pictureId);
+        checkBoxes.put(key,checkboxRowCategory);
+        checkboxRowCategory.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    for (LinearLayout linearLayout : Categories.get(key)) {
+                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+                        newCheck.setChecked(true);
+
+
+                    }
+                }else {
+                    for (LinearLayout linearLayout : Categories.get(key)) {
+                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+                        newCheck.setChecked(false);
+                    }
+                }
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onclickDropDownList(v,key);
+            }
+        });
+        linearLayoutParents.put(key,linearLayout);
+        seeIfAllCheckboxIsChecked(checkboxRowCategory,key);
+        return linearLayout;
+    }
+
+    private void seeIfAllCheckboxIsChecked(CheckBox checkboxRowCategory, String key) {
+        for (LinearLayout linearLayout : Categories.get(key)) {
+            CheckBox checkBox = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+            Log.d(TAG, String.valueOf(checkBox.isChecked()));
+            if(!checkBox.isChecked()){
+                checkboxRowCategory.setChecked(false);
+                return;
+            }
+        }
+        checkboxRowCategory.setChecked(true);
+
+    }
+
+
+    private void addCategory(LayoutInflater inflater, ArrayList<AllergyList.PictureIngredient> arrayListCategory, final String key){
+        ArrayList<LinearLayout> arrayList = new ArrayList<>();
+        for (final AllergyList.PictureIngredient arrayListCat : arrayListCategory) {
+            LinearLayout newLinearLayout = (LinearLayout) inflater.inflate(R.layout.leftmarginrowlayout,null);
+            TextView textview = (TextView) newLinearLayout.findViewById(R.id.textViewLeftMargin);
+            textview.setText(arrayListCat.ingredient);
+            ImageView imageView = (ImageView) newLinearLayout.findViewById(R.id.imageViewLeftMargin);
+            imageView.setImageResource(arrayListCat.picture);
+            SharedPreferences settings = getContext().getSharedPreferences(arrayListCat.ingredient, 0);
+            final SharedPreferences.Editor editor = settings.edit();
+            CheckBox checkBox = (CheckBox) newLinearLayout.findViewById(R.id.checkBoxRow);
+            checkBox.setChecked(settings.getBoolean(arrayListCat.ingredient, false));
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    CheckBox checkbox = checkBoxes.get(key);
+
+                        checkbox.setOnCheckedChangeListener(null);
+                        seeIfAllCheckboxIsChecked(checkbox,key);
+                        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if(isChecked) {
+                                    for (LinearLayout linearLayout : Categories.get(key)) {
+                                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+                                        newCheck.setChecked(true);
+
+
+                                    }
+                                }else {
+                                    for (LinearLayout linearLayout : Categories.get(key)) {
+                                        CheckBox newCheck = (CheckBox) linearLayout.findViewById(R.id.checkBoxRow);
+                                        newCheck.setChecked(false);
+                                    }
+                                }
+                            }
+                        });
+
+                    editor.putBoolean(arrayListCat.ingredient, isChecked);
+                    editor.apply();
+                }
+            });
+            arrayList.add(newLinearLayout);
+        }
+        Categories.put(key,arrayList);
+
+    }
+
+
+
+    public void onclickDropDownList(View v,final String key) {
         if(v.getRotation() == 180){
             v.setRotation(0);
+            for (LinearLayout linearLayout : Categories.get(key)) {
+                linearLayoutParents.get(key).removeView(linearLayout);
 
-            for (LinearLayout linearLayout : nutsCategory) {
-
-                parentLinearLayout.removeView(linearLayout);
             }
         }else{
             v.setRotation(180);
-            for (LinearLayout linearLayout : nutsCategory) {
-                parentLinearLayout.addView(linearLayout);
+
+            for (LinearLayout linearLayout : Categories.get(key)) {
+                linearLayoutParents.get(key).addView(linearLayout);
 
             }
+
 
         }
 
--- app/src/main/java/creativeendlessgrowingceg/allergychecker/AllergyList.java
+++ app/src/main/java/creativeendlessgrowingceg/allergychecker/AllergyList.java
@@ -3,6 +3,8 @@ package creativeendlessgrowingceg.allergychecker;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by Enathen on 2017-10-13.
@@ -12,10 +14,94 @@ public class AllergyList {
     private static final String TAG = "ArrayListAllergy";
     private ArrayList<String> arrayListIngredients = new ArrayList<>();
     private ArrayList<ImageView> arrayListPictures = new ArrayList<>();
-    private ArrayList<String> arrayListNuts = new ArrayList<>();
-    private ArrayList<String> arrayListGluten = new ArrayList<>();
+    private ArrayList<PictureIngredient> arrayListNuts = new ArrayList<>();
+    private ArrayList<PictureIngredient> arrayListGluten = new ArrayList<>();
+    private ArrayList<PictureIngredient> arrayListFish = new ArrayList<>();
+    private ArrayList<PictureIngredient> arrayListShellfish = new ArrayList<>();
+    private ArrayList<PictureIngredient> arrayListSeeds= new ArrayList<>();
     public AllergyList(){
-        setArrayListIngredients();
+        setArrayListNuts();
+        setArrayListGluten();
+        setArrayListFish();
+        setArrayListShellfish();
+        setArrayListSeeds();
+    }
+
+    public ArrayList<PictureIngredient> getArrayListSeeds() {
+        return arrayListSeeds;
+    }
+
+    public void setArrayListSeeds() {
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Sesame"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Benne"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Gingelly"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Gomasio"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Halvah"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Sim sim"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Tahini"));
+        arrayListSeeds.add(new PictureIngredient(R.drawable.fish,"Til"));
+    }
+
+    public ArrayList<PictureIngredient> getArrayListFish() {
+        return arrayListFish;
+    }
+
+    public void setArrayListFish() {
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Pike"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Tuna"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Salmon"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Caviar"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Anchovies"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Bass"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Catfish"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Cod"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Flounder"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Grouper"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Haddock"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Hake"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Halibut"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Herring"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Mahi"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Perch"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Pollock"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Pike"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Swordfish"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Sole"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Snapper"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Surimi"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Tilapia"));
+        arrayListFish.add(new PictureIngredient(R.drawable.fish,"Trout"));
+        sort(arrayListFish);
+    }
+
+    public ArrayList<PictureIngredient> getArrayListShellfish() {
+        return arrayListShellfish;
+    }
+
+    public void setArrayListShellfish() {
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Barnacle"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Crab"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Crawfish"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Crayfish"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Krill"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Lobster"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Prawns"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Shrimp"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"scampi"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Abalone"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Clams"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Cockle"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Limpet"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Mussels"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Octopus"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Oysters"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Periwinkle"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Sea cucumber"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Sea urchin"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Scallops"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Snails"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Squid"));
+        arrayListShellfish.add(new PictureIngredient(R.drawable.fish,"Whelk"));
     }
 
     public ArrayList<String> getArrayListIngredients() {
@@ -26,33 +112,70 @@ public class AllergyList {
         return arrayListPictures;
     }
 
-    public ArrayList<String> getArrayListNuts() {
+    public ArrayList<PictureIngredient> getArrayListNuts() {
         return arrayListNuts;
     }
 
-    public ArrayList<String> getArrayListGluten() {
+    public ArrayList<PictureIngredient> getArrayListGluten() {
         return arrayListGluten;
     }
 
-    public void setArrayListIngredients() {
-        arrayListNuts.add("Almond");
-        arrayListNuts.add("Brazil nuts");
-        arrayListNuts.add("Cacao");
-        arrayListNuts.add("Hazelnut");
-        arrayListNuts.add("Mmacadamia nuts");
-        arrayListNuts.add("Peanuts");
-        arrayListNuts.add("Pecans");
-        arrayListNuts.add("Pili nuts");
-        arrayListNuts.add("Pine nuts");
-        arrayListNuts.add("Pistachios");
-        arrayListNuts.add("Pistachios");
-        arrayListNuts.add("Soy nuts");
-        arrayListNuts.add("Tiger nuts");
-        arrayListNuts.add("Walnuts");
+    public void setArrayListNuts() {
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Almond"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Brazil nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Cacao"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Hazelnut"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Macadamia nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Peanuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Pecans"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Pili nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Pine nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Pistachios"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Pistachios"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Soy nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Tiger nuts"));
+        arrayListNuts.add(new PictureIngredient(R.drawable.peanut,"Walnuts"));
+        sort(arrayListNuts);
+    }
+
+    public void setArrayListGluten() {
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Wheat"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Durum"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Emmer"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Rye"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Barley"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Triticale"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Malt"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Semolina"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Spelt"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Farina"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Farro"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Graham"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"KAMUT"));
+        arrayListGluten.add(new PictureIngredient(R.drawable.wheat,"Einkorn"));
+        sort(arrayListGluten);
 
     }
 
     public void setArrayListPictures(String arrayListPictures) {
         this.arrayListIngredients.add(arrayListPictures);
+    }
+    private void sort(ArrayList<PictureIngredient> arrayList){
+        Collections.sort(arrayList, new Comparator<PictureIngredient>() {
+            @Override
+            public int compare(PictureIngredient pic1, PictureIngredient pic2) {
+                return pic2.ingredient.compareToIgnoreCase(pic1.ingredient);
+            }
+        });
+    }
+    public class PictureIngredient{
+        int picture;
+        String ingredient;
+
+        PictureIngredient(int picture, String ingredient){
+            this.picture = picture;
+
+            this.ingredient = ingredient;
+        }
     }
 }
